96585ac5799e8d6c8e6781f79dd21355282ec95d
Preston <prestonosoro56@gmail.com>
2023-02-13 11:59:07 +0300

    feat:instantiate

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,24 @@
+class Book
+    #define macro variable
+    attr_accessor :title,:author,:page_count,:genre
+    #define initialize title method
+    def initialize(title)
+        @title = title
+    end
+    #define turn-page method
+    def turn_page
+        puts "Flipping the page...wow, you read fast!"
+    end
+end
+#create instance of new Bok class
+books = Book.new("And Then There Were None")
+#put authors in books
+puts books.author = "Agatha Christie"
+#put page count in books
+puts books.page_count = 272
+#put genre in books
+puts books.genre = "Mystery"
+#turn page method
+puts books.turn_page
 
 
